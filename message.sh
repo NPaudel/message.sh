cc588859e50178c54a4e0f90b16411373ecb4253
Nobel <54449465+NPaudel@users.noreply.github.com>
2022-01-30 19:00:52 +0545

    Got it working with cron and at
    Signed-off-by: Nobel <54449465+NPaudel@users.noreply.github.com>

--- message.sh
+++ message.sh
@@ -1,12 +1,10 @@
-#!/bin/bash
-
-firefox <url>
+export DISPLAY=:1 &&  /usr/bin/firefox google.com
 # Put in the URL of the chat where you want to send the message 
 # This script has been tested with Messenger and Discord till now
 
-sleep 5 # to let the page load
+sleep 10 # to let the page load
 
 xdotool type "Test Message" # type the message you want to send
 xdotool key 0xff0d
-
+sleep 5 # to let the message send
 pkill -f firefox # close firefox after the message is sent
